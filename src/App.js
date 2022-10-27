5be05ae729390d09f1574e37f6f2b08ad2af3efc
zuechai <zuechai@gmail.com>
2022-10-27 18:45:30 -0400

    Add to gitignore

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import "./App.scss";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage/Homepage';
-import PageHeader from './components/PageHeader/PageHeader';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/Homepage";
+import PageHeader from "./components/PageHeader/PageHeader";
 
 function App() {
-  return (<>
-    <div className='App'>
-      <Router>
-        <PageHeader />
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-        </Routes>
-      </Router>
-    </div>
-  </>)
+  return (
+    <>
+      <div>
+        <Router>
+          <PageHeader />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </Router>
+      </div>
+    </>
+  );
 }
 
 export default App;
