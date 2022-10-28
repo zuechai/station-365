9f9a60857c310a32b94c959f40ab66bba0c8febc
zuechai <zuechai@gmail.com>
2022-10-28 15:32:23 -0400

    style

--- src/components/TicketCard/TicketCard.js
+++ src/components/TicketCard/TicketCard.js
@@ -1,24 +1,26 @@
-import { useState, useEffect } from "react";
-import { getAllUsers } from "../../utils/apiCalls";
 import "./TicketCard.scss";
 
 export default function TicketCard({ ticket }) {
   return (
     <>
-      <div className="ticket__title ">
-        <p>{ticket.title}</p>
-      </div>
-      <div className="ticket__description">
-        <p>{ticket.description}</p>
-      </div>
-      <div className="ticket__votes-for">
-        <p>{ticket.votesFor.length}</p>
-      </div>
-      <div className="ticket__votes-against">
-        <p>{ticket.votesAgainst.length}</p>
-      </div>
-      <div className="ticket__resolved">
-        <p>{ticket.isResolved ? "Open" : "Closed"}</p>
+      <div className="ticket">
+        <div className="ticket-rows">
+          <div className="ticket__title ">
+            <p>{ticket.subject}</p>
+          </div>
+          <div className="ticket__description">
+            <p>{ticket.description}</p>
+          </div>
+          <div className="ticket__votes-for">
+            <p>{ticket.votesFor.length}</p>
+          </div>
+          <div className="ticket__votes-against">
+            <p>{ticket.votesAgainst.length}</p>
+          </div>
+          <div className="ticket__resolved">
+            <p>{ticket.isResolved ? "Open" : "Closed"}</p>
+          </div>
+        </div>
       </div>
     </>
   );
